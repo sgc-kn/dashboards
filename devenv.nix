854306aad37c9464fcce9a6284492734f69fb7e4
Patrik Keller <git@pkel.dev>
2024-11-28 15:27:44 +0100

    Reproducible dev environment (#18)
    Setup devenv.sh environment for managing dependencies
    - pin python and node/npm versions (devenv.lock)
    - pin python package versions (requirements.txt)
    - pin npm package versions (package-lock.json) (not new)
    - use devenv in CI/CD
    - support `devenv up`
    
    Use `devenv update` to install the pinned package versions. Use `devenv upgrade` to update the lock files with new package versions.

---
+++ devenv.nix
@@ -0,0 +1,75 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.DEVENV = "sgc-kn/dashboards";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  languages.javascript.enable = true;
+  languages.javascript.npm.enable = true;
+  languages.python.enable = true;
+  languages.python.venv.enable = true;
+  languages.python.venv.requirements = "pip-tools";
+
+  # https://devenv.sh/processes/
+  processes.preview = {
+    exec = "npm run dev";
+    process-compose =  {
+      availability = {
+        backoff_seconds = 5;
+        restart = "on_failure";
+      };
+    };
+  };
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.update.exec = ''
+    # install dependencies as defined in the lock files
+
+    echo install javascript dependencies from package-lock.json
+    npm clean-install
+
+    echo install python dependencies from requirements.txt
+    pip-sync
+  '';
+
+  scripts.upgrade.exec = ''
+    # update lock files with newest versions & install
+
+    echo update npm packages and package-lock.json
+    npm update
+
+    echo update requirements.txt
+    pip-compile --upgrade --strip-extras --quiet
+
+    echo install python dependencies from requirements.txt
+    pip-sync
+  '';
+
+  enterShell = ''
+    update
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  # enterTest = ''
+  #   echo "Running tests"
+  #   git --version | grep --color=auto "${pkgs.git.version}"
+  # '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
